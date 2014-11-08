4557a0478c7c053e343baee78725ae2bf3306a69
Belmin Fernandez <me@iambelmin.com>
2014-11-08 15:38:32 -0500

    adding more logic to rhn check

--- rhn.py
+++ rhn.py
@@ -1,5 +1,11 @@
 from fabric.api import *
 
 def rhn_check():
-    if run('ls /etc/sysconfig/rhn/systemid', quiet=True).return_code != 0:
+    dns_domain = local('hostname -d', capture=True).strip()
+    fqdn = '.' in env.host and env.host or '{host}.{dns_domain}'.format(host=env.host, dns_domain=dns_domain)
+    puts(fqdn)
+    results = sudo('grep {fqdn} /etc/sysconfig/rhn/systemid'.format(**locals()), quiet=True)
+    if results.return_code == 1:
+        puts('Registered as another hostname')
+    elif results.return_code == 2:
         puts('Not registered')
