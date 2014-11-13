b24b101e6be9a0ce999fedff9f37872ef979a8f7
Belmin Fernandez <me@iambelmin.com>
2014-11-13 09:44:51 -0500

    added rhn_register

--- rhn.py
+++ rhn.py
@@ -1,11 +1,17 @@
 from fabric.api import *
 
+DNS_DOMAIN = local('hostname -d', capture=True).strip()
+
 def rhn_check():
-    dns_domain = local('hostname -d', capture=True).strip()
-    fqdn = '.' in env.host and env.host or '{host}.{dns_domain}'.format(host=env.host, dns_domain=dns_domain)
+    fqdn = '.' in env.host and env.host or '{host}.{dns_domain}'.format(host=env.host, dns_domain=DNS_DOMAIN)
     puts(fqdn)
     results = sudo('grep {fqdn} /etc/sysconfig/rhn/systemid'.format(**locals()), quiet=True)
     if results.return_code == 1:
-        puts('Registered as another hostname')
+        puts('RHNCHECK: Registered as another hostname')
     elif results.return_code == 2:
-        puts('Not registered')
+        puts('RHNCHECK: Not registered')
+
+def rhn_register(key, server=None, server_path='/XMLRPC', force=True):
+    server_param = server and ' --serverUrl https://{0}{1}'.format(server, server_path) or ''
+    force_param = force and ' --force' or ''
+    sudo('/usr/sbin/rhnreg_ks --activationkey {0}{1}{2}'.format(key, server_param, force_param))
