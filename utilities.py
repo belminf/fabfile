b95b36df4d99f1b1824848b0a08711da937b5793
Belmin Fernandez <me@iambelmin.com>
2015-02-27 09:16:50 -0500

    added sudo_cmd

--- utilities.py
+++ utilities.py
@@ -2,7 +2,7 @@ from fabric.api import *
 import sys
 
 def read_hosts():
-    env.hosts = [ h.strip() for h in sys.stdin.readlines() if h.strip() and not h.strip().startswith(('#','/')) ]
+    env.hosts = [ h.strip().split(' ')[0] for h in sys.stdin.readlines() if h.strip() and not h.strip().startswith(('#','/')) ]
 
-def sudo_su():
-    sudo('su -')
+def sudo_cmd(command):
+    sudo(command)
